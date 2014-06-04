3cc86bb0f914e8474611ac537155f9520633c398
DennisSandig <Dennis.Sandig@gmx.de>
2014-06-04 21:17:53 +0200

    mainpage erstellt

---
+++ public/javascripts/index-js.js
@@ -0,0 +1,15 @@
+//$( document ).ready(function() {
+//  
+//});
+
+$(".field").mousedown(function(){
+	alert("Nachricht");
+});
+
+$(".field").click(function(){
+	alert("Nachricht");
+});
+
+function vnchange(feld) {
+	$("#vnamedialog").dialog();
+}
