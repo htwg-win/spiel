9c05663a2ceb22b8db66859a56228919baaf4017
DennisSandig <Dennis.Sandig@gmx.de>
2014-06-06 01:29:10 +0200

    Positionierung des maincontent angepasst -> responsive Design

--- public/javascripts/index-js.js
+++ public/javascripts/index-js.js
@@ -1,20 +1,21 @@
 var $d;
 
 $().ready(function() {
-	$("#popupuser").dialog({ hide: { effect: "explode", duration: 1000 } });
-	$("#startgame").on('click touchstart', function(){
-		$("#popupuser").dialog( "close" )
-		$("#maincontent").show("explode", "slow");
-//		$("#maincontent").css("visibility", "visible");
-	})
+//	$("#popupuser").dialog({ hide: { effect: "explode", duration: 1000 } });
+//	$("#startgame").on('click touchstart', function(){
+//		$("#popupuser").dialog( "close" )
+//		$("#maincontent").show("explode", "slow");
+//	})
 	
 	$d = $(document);
-	
 	$d.on('click touchstart','.fields',function(e){
 				
 		alert(e.target.dataset.number);
 	});
 	
-	
+//	var cw = $('.fields').width();
+//	$('.fields').css({
+//	    'height': cw + '%'
+//	});
 	
 });
