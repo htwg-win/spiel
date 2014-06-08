d77d69342eddcffa2e945f007cae01e09fb54f81
DennisSandig <Dennis.Sandig@gmx.de>
2014-06-08 20:24:02 +0200

    Login, Logout funktioniert Responsive Design mit media queries JS angepasst Maincontainer entfernt Positionierung für Responsive Design angepasst

--- public/javascripts/index-js.js
+++ public/javascripts/index-js.js
@@ -3,12 +3,19 @@ var $d;
 $().ready(function() {
 	$("#login").on('click touchstart', function(){
 		$(".login").hide()
-		$("#maincontent").show();
+		$("#fields").show()
+		$("#menu").show();
 	})
-	
 	$("#create").on('click touchstart', function(){
 		$(".login").hide()
-		$("#maincontent").show();
+		$("#fields").show()
+		$("#menu").show();
+	})
+	
+	$("#logout").on('click touchstart', function(){
+		$(".login").show()
+		$("#fields").hide()
+		$("#menu").hide();
 	})
 	
 	$d = $(document);
