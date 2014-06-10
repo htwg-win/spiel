7f476e272e40bc02b15849bbf4c113e4d6200e53
DennisSandig <Dennis.Sandig@gmx.de>
2014-06-10 15:37:12 +0200

    Database Create

--- app/database/databasecon.java
+++ app/database/databasecon.java
@@ -1,5 +1,27 @@
 package database;
 
-public class databasecon {
+import java.sql.*;
 
+public class databasecon {
+	public void create() {
+		String name = "";
+		String password = "";
+		
+		try {
+			Connection conn = DriverManager.getConnection("jdbc:sqlite:\\SQLite\\game.db3");
+			Statement stmt = conn.createStatement();
+			ResultSet test = stmt.executeQuery("SELECT userid FROM user");
+			
+			
+			while(test.next())
+				System.out.println(test.getString("userid"));
+			
+//			stmt.execute(sql);
+			stmt.close();
+			conn.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+	}
 }
